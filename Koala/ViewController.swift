b15420e266bbdb334808bddb36579911e10edbc8
David Miotti <david@muxumuxu.com>
2017-04-07 18:56:38 +0200

    Initial Commit

---
+++ Koala/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Koala
+//
+//  Created by David Miotti on 07/04/2017.
+//  Copyright © 2017 Muxu.Muxu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
