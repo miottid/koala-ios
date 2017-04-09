4e3e798b3f1b6e1f8f7515d9fff902e3ddaf5158
David Miotti <david@muxumuxu.com>
2017-04-09 14:53:42 +0200

    Update footer in settings page

--- Koala/SettingsFooterView.swift
+++ Koala/SettingsFooterView.swift
@@ -11,21 +11,27 @@ import SwiftHelpers
 
 final class SettingsFooterView: SHCommonInitView {
     private var faceImageView = UIImageView(image: #imageLiteral(resourceName: "koala_face"))
+    private var personLbl = UILabel()
     private var madeWithLoveLbl = UILabel()
     private var versionLbl = UILabel()
 
     override func commonInit() {
         super.commonInit()
         addSubview(faceImageView)
+        personLbl.text = L("footer.sleep_well")
+        personLbl.font = .systemFont(ofSize: 11, weight: UIFontWeightMedium)
+        personLbl.textColor = .white
+        personLbl.textAlignment = .center
         madeWithLoveLbl.text = L("footer.made_with_love")
-        madeWithLoveLbl.textColor = .white
+        madeWithLoveLbl.textColor = UIColor.white.withAlphaComponent(0.6)
         madeWithLoveLbl.textAlignment = .center
         madeWithLoveLbl.font = .systemFont(ofSize: 11)
-        addSubview(madeWithLoveLbl)
         versionLbl.text = "v\(appVersion())"
         versionLbl.textAlignment = .center
         versionLbl.textColor = UIColor.white.withAlphaComponent(0.6)
         versionLbl.font = .systemFont(ofSize: 11)
+        addSubview(personLbl)
+        addSubview(madeWithLoveLbl)
         addSubview(versionLbl)
         configureLayoutConstraints()
     }
@@ -35,8 +41,12 @@ final class SettingsFooterView: SHCommonInitView {
             $0.top.equalToSuperview()
             $0.centerX.equalToSuperview()
         }
-        madeWithLoveLbl.snp.makeConstraints {
+        personLbl.snp.makeConstraints {
             $0.top.equalTo(faceImageView.snp.bottom).offset(3)
+            $0.left.right.equalToSuperview()
+        }
+        madeWithLoveLbl.snp.makeConstraints {
+            $0.top.equalTo(personLbl.snp.bottom).offset(3)
             $0.left.right.equalToSuperview()
         }
         versionLbl.snp.makeConstraints {
